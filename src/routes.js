fc6dd28850a8bc6fafd43df918bd48dbd04a8a82
rmwdeveloper <rmwdeveloper@gmail.com>
2016-04-17 20:07:42 -0500

    Delete a lot of stuff

--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,7 @@ import {IndexRoute, Route} from 'react-router';
 import { isLoaded as isAuthLoaded, load as loadAuth } from 'redux/modules/auth';
 import {
     App,
-    Chat,
     Home,
-    Widgets,
-    About,
-    Login,
-    LoginSuccess,
-    Survey,
     NotFound,
   } from 'containers';
 
@@ -38,19 +32,7 @@ export default (store) => {
     <Route path="/" component={App}>
       { /* Home (main) route */ }
       <IndexRoute component={Home}/>
-
-      { /* Routes requiring login */ }
-      <Route onEnter={requireLogin}>
-        <Route path="chat" component={Chat}/>
-        <Route path="loginSuccess" component={LoginSuccess}/>
-      </Route>
-
-      { /* Routes */ }
-      <Route path="about" component={About}/>
-      <Route path="login" component={Login}/>
-      <Route path="survey" component={Survey}/>
-      <Route path="widgets" component={Widgets}/>
-
+      
       { /* Catch all route */ }
       <Route path="*" component={NotFound} status={404} />
     </Route>
