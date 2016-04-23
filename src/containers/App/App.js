547f04e070334d3fff66f374d15b8ce937c072b8
rmwdeveloper <rmwdeveloper@gmail.com>
2016-04-23 15:02:25 -0500

    Add navbar

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,7 +1,9 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { IndexLink } from 'react-router';
+import { LinkContainer } from 'react-router-bootstrap';
 import Helmet from 'react-helmet';
-
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import { routeActions } from 'react-router-redux';
 import config from '../../config';
 
@@ -36,6 +38,25 @@ export default class App extends Component {
     return (
       <div className={styles.app}>
         <Helmet {...config.app.head}/>
+        <Navbar fluid>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <IndexLink to="/" activeStyle={{color: '#33e0ff'}}>
+                <div className={styles.brand}/>
+                <span>{config.app.title}</span>
+              </IndexLink>
+            </Navbar.Brand>
+            <Navbar.Toggle/>
+          </Navbar.Header>
+
+          <Navbar.Collapse eventKey={0}>
+            <Nav navbar>
+              <LinkContainer to="/about">
+                <NavItem eventKey={1}>About Us</NavItem>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
         <div>
           {this.props.children}
         </div>
