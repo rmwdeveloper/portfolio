2538aedfffc551d6276d23db5e7bf854f0ce4302
rmwdeveloper <rmwdeveloper@gmail.com>
2016-04-24 13:32:34 -0500

    Add about me route and container. Remove margin from navbar

---
+++ src/containers/About/About.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+export default class Home extends Component {
+  render() {
+    const styles = require('./Home.scss');
+    // require the logo image both from client and server
+    // const logoImage = require('./logo.png');
+    return (
+      <div>
+        Hey whats up 
+      </div>
+    );
+  }
+}
