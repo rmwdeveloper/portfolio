0a27a0da463ecaf27948c93959949609f8f4a920
rmwdeveloper <rmwdeveloper@gmail.com>
2016-04-24 09:42:05 -0500

    Add ref to rendered div

--- src/components/FlatSurfaceShader/FlatSurfaceShader.js
+++ src/components/FlatSurfaceShader/FlatSurfaceShader.js
@@ -1,8 +1,7 @@
 import React, {Component, PropTypes} from 'react';
 
 
-export default
-class FlatSurfaceShader extends Component {
+export default class FlatSurfaceShader extends Component {
 
   constructor(props) {
     super(props);
@@ -12,11 +11,19 @@ class FlatSurfaceShader extends Component {
     this.numPointsY = Math.ceil(window.innerHeight / this.unitSize) + 1;
     this.unitWidth = Math.ceil(window.innerWidth / (this.numPointsX - 1));
     this.unitHeight = Math.ceil(window.innerHeight / (this.numPointsY - 1));
-    this.refreshTimeout = null;
+    this.refreshTimeout = 10000;
     this.randomize = this.randomize.bind(this);
     this.refresh = this.refresh.bind(this);
     this.onResize = this.onResize.bind(this);
+    this.onLoad = this.onLoad.bind(this);
   }
+  componentDidMount() {
+    window.onload = this.onLoad;
+    window.onresize = this.onResize;
+  }
+  /*
+   onLoad() creates an svg and appends it to the div rendered by this component.
+   */
   onLoad() {
     let y = 0;
     let x = 0;
@@ -26,8 +33,7 @@ class FlatSurfaceShader extends Component {
     const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
     svg.setAttribute('width', window.innerWidth);
     svg.setAttribute('height', window.innerHeight);
-    document.querySelector('#bg').appendChild(svg);
-
+    this._bg.appendChild(svg);
     for (y; y < numPointsY; y++) {
       for (x; x < numPointsX; x++) {
         points.push({x: unitWidth * x, y: unitHeight * y, originX: unitWidth * x, originY: unitHeight * y});
@@ -35,6 +41,8 @@ class FlatSurfaceShader extends Component {
     }
 
     this.randomize();
+
+    console.log('points', points);
 
     for (i; i < points.length; i++) {
       if (points[i].originX !== unitWidth * (numPointsX - 1) && points[i].originY !== unitHeight * (numPointsY - 1)) {
@@ -48,7 +56,6 @@ class FlatSurfaceShader extends Component {
         const bottomRightY = points[i + numPointsX + 1].y;
 
         const random = Math.floor(Math.random() * 2);
-
         for (n; n < 2; n++) {
           const polygon = document.createElementNS(svg.namespaceURI, 'polygon');
 
@@ -62,14 +69,14 @@ class FlatSurfaceShader extends Component {
               polygon.point1 = i;
               polygon.point2 = i + 1;
               polygon.point3 = i + numPointsX + 1;
-              polygon.setAttribute('points' , topLeftX + ',' + topLeftY + ' ' + topRightX + ',' + topRightY + ' ' + bottomRightX + ',' + bottomRightY);
+              polygon.setAttribute('points', topLeftX + ',' + topLeftY + ' ' + topRightX + ',' + topRightY + ' ' + bottomRightX + ',' + bottomRightY);
             }
           } else if (random === 1) {
             if (n === 0) {
               polygon.point1 = i;
               polygon.point2 = i + numPointsX;
               polygon.point3 = i + 1;
-              polygon.setAttribute('points' , topLeftX + ',' + topLeftY + ' ' + bottomLeftX + ',' + bottomLeftY + ' ' + topRightX + ',' + topRightY);
+              polygon.setAttribute('points', topLeftX + ',' + topLeftY + ' ' + bottomLeftX + ',' + bottomLeftY + ' ' + topRightX + ',' + topRightY);
             } else if (n === 1) {
               polygon.point1 = i + numPointsX;
               polygon.point2 = i + 1;
@@ -81,7 +88,7 @@ class FlatSurfaceShader extends Component {
           const animate = document.createElementNS('http://www.w3.org/2000/svg', 'animate');
           animate.setAttribute('fill', 'freeze');
           animate.setAttribute('attributeName', 'points');
-          animate.setAttribute('dur', refreshDuration + 'ms');
+          animate.setAttribute('dur', this.refreshDuration + 'ms');
           animate.setAttribute('calcMode', 'linear');
           polygon.appendChild(animate);
           svg.appendChild(polygon);
@@ -91,7 +98,7 @@ class FlatSurfaceShader extends Component {
     this.refresh();
   }
   onResize() {
-    document.querySelector('#bg svg').remove();
+    this._bg.remove();
     clearTimeout(this.refreshTimeout);
     this.onLoad();
   }
@@ -111,8 +118,8 @@ class FlatSurfaceShader extends Component {
     let i = 0;
     const { points } = this;
     this.randomize();
-    for ( i; i < document.querySelector('#bg svg').childNodes.length; i++) {
-      const polygon = document.querySelector('#bg svg').childNodes[i];
+    for ( i; i < this._bg.childNodes.length; i++) {
+      const polygon = this._bg.childNodes[i];
       const animate = polygon.childNodes[0];
       if (animate.getAttribute('to')) {
         animate.setAttribute('from', animate.getAttribute('to'));
@@ -122,13 +129,12 @@ class FlatSurfaceShader extends Component {
         ',' + points[polygon.point3].y);
       animate.beginElement();
     }
-    this.refreshTimeout = setTimeout(function(){refresh();}, this.refreshDuration);
+    this.refreshTimeout = setTimeout(() => {this.refresh();}, this.refreshDuration);
   }
   render() {
-    const { refreshDuration } = this.props;
     const styles = require('./FlatSurfaceShader.scss');
     return (
-      <div className={styles.flatSurfaceShader}></div>
+      <div ref={(c) => this._bg = c} className={styles.flatSurfaceShader}></div>
     );
   }
 }
